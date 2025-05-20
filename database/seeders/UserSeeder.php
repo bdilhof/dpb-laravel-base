246ab9871a8d04673893ee1336c113181980571c
Bruno Dilhof <brunodilhof@gmail.com>
2025-05-20 08:54:44 +0200

    First commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $admin = User::create([
+            'firstname' => 'Admin',
+            'lastname' => 'DPB',
+            'login' => 'admin',
+            'color' => '#000000',
+            'email' => 'dilhof.bruno@dpb.sk',
+            'password' => \Hash::make('0000'),
+            'is_active' => 1,
+            'first_login' => 1,
+        ]);
+
+        $admin->assignRole('super-admin');
+    }
+}
